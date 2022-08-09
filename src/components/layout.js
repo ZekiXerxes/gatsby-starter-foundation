dbbacc0223b28394ef1c3d036b6dc6462b0e94ae
ZekiXerxes <56410159+ZekiXerxes@users.noreply.github.com>
2022-08-10 01:26:23 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,6 @@ const query = graphql`
     site {
       siteMetadata {
         siteTitle: title
-        logoImage: image
       }
     }
     siteSearchIndex {
@@ -32,7 +31,7 @@ const Layout = ({ children, className, props }) => {
   return (
     <div className="primary-container">
       <Header>
-        <Logo title={siteTitle + logoImage} />
+        <Logo title={siteTitle} />
         <div sx={layoutStyle.nav}>
           <div sx={{ display: ["flex", "flex", "flex", "none"] }}>
             <Search searchIndex={siteSearchIndex.index} />
